15e672584d9576f40e904c396b2ef770827b1d21
chris marx <chris@zevross.com>
2021-09-29 11:00:24 -0300

    initial commit

---
+++ deck-layers-app/src/components/BackdropMap/BackdropMap.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './BackdropMap.module.scss';
+
+const BackdropMap = () => (
+  <div className={styles.BackdropMap} data-testid="BackdropMap">
+    BackdropMap Component
+  </div>
+);
+
+export default BackdropMap;
